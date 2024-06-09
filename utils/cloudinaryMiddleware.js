a061e444b311f13587ab2a7185d48ef4f61fbb54
ChuloCr8v <chulocr8v@gmail.com>
2024-06-09 20:22:40 +0100

    updated

--- utils/cloudinaryMiddleware.js
+++ utils/cloudinaryMiddleware.js
@@ -1,8 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 
-const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
-const api_key = process.env.CLOUDINARY_API_KEY;
-const api_secret = process.env.CLOUDINARY_API_SECRET;
+// const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
+// const api_key = process.env.CLOUDINARY_API_KEY;
+// const api_secret = process.env.CLOUDINARY_API_SECRET;
 
 export const cloudinaryProps = cloudinary.config({
   cloud_name: "dzcave05f",
